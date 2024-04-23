73e30c44973fc3362a2e063198f1d4e0ec9f59d2
Linden Schrage <lindenws@gmail.com>
2024-04-22 20:59:06 -0400

    add

--- cs109b_llama_regression_head.py
+++ cs109b_llama_regression_head.py
@@ -137,8 +137,8 @@ X_train, X_val, y_train, y_val = train_test_split(
 
 lr_schedule = keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate=1e-6,
-    decay_steps=10000,
-    decay_rate=0.8)
+    decay_steps=20000,
+    decay_rate=0.7)
 
 early_stopping_monitor = EarlyStopping(
     monitor='val_loss',
